f63ea39a7d3ca484b7f22ac8299501f4f65e5a15
Adam Damiano <onaimada@gmail.com>
2021-07-03 12:41:29 -0700

    Make "add link" button look nice

--- components/Row.js
+++ components/Row.js
@@ -11,7 +11,13 @@ export default function Row({ children, rowNum, isLowestRow }) {
   return (
     <div className="gap-2 flex flex-wrap">
       {children}
-      <button onClick={() => dispatch(addItemAtEndOfRow(rowNum))}>➕</button>
+
+      <button
+        className="bg-indigo-50 text-indigo-900 cursor-pointer px-4 py-3 border border-indigo-300 rounded space-x-2 flex items-center h-14"
+        onClick={() => dispatch(addItemAtEndOfRow(rowNum))}
+      >
+        <i className="las la-link"></i> Add link
+      </button>
       <button onClick={() => dispatch(deleteRow(rowNum))}>❌</button>
       <button
         disabled={rowNum === 0}
