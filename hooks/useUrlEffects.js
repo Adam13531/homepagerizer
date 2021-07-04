711aba02417a224af9d055cc9aa75f073d007f71
Adam Damiano <onaimada@gmail.com>
2021-07-03 21:29:51 -0700

    Start a new homepage with a single item
    This should make the new-user UI a little less bare.

--- hooks/useUrlEffects.js
+++ hooks/useUrlEffects.js
@@ -1,7 +1,14 @@
 import { useEffect } from "react";
 import _ from "lodash";
-import { useDispatch } from "react-redux";
-import { setHomepagerizerAddress } from "../state/contentSlice";
+import { useDispatch, useSelector } from "react-redux";
+
+import {
+  addRow,
+  addItemAtEndOfRow,
+  updateItem,
+  setHomepagerizerAddress,
+  selectRows,
+} from "../state/contentSlice";
 import { loadSavedState } from "../state/actions";
 
 /**
@@ -10,6 +17,7 @@ import { loadSavedState } from "../state/actions";
  */
 export default function useUrlEffects() {
   const dispatch = useDispatch();
+  const rows = useSelector(selectRows);
   useEffect(() => {
     const homepagerizerAddress = `${location.protocol}//${location.host}`;
     dispatch(setHomepagerizerAddress(homepagerizerAddress));
@@ -21,6 +29,21 @@ export default function useUrlEffects() {
       const jsonStr = atob(base64Json);
       const jsonObj = JSON.parse(jsonStr);
       dispatch(loadSavedState(jsonObj));
+    } else {
+      // Start the user out with some content so that they know what to expect.
+      //
+      // The .isEmpty is only needed during development so that hot-reloading
+      // doesn't cause this item to repeatedly get added every time there's a
+      // code change.
+      if (_.isEmpty(rows)) {
+        dispatch(addRow());
+        dispatch(addItemAtEndOfRow(0));
+        dispatch(
+          updateItem(0, 0, {
+            text: "Your first link",
+          })
+        );
+      }
     }
   }, []);
 }
