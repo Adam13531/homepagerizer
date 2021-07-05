5611464bcece231bcc404241b832288d3cd954da
Adam Damiano <onaimada@gmail.com>
2021-07-05 10:48:02 -0700

    Add open-graph metadata

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,22 @@ import Head from "next/head";
 import store from "../state/store";
 import { Provider } from "react-redux";
 import App from "../components/App";
+import { NextSeo } from "next-seo";
 
 export default function Home() {
   return (
     <>
+      <NextSeo
+        title="Homepagerizer"
+        openGraph={{
+          type: "website",
+          locale: "en_US",
+          title: "Homepagerizer",
+          url: "https://homepagerizer.vercel.app",
+          description: "Make simple, fast homepages completely for free.",
+        }}
+      />
       <Head>
-        <title>Homepagerizer</title>
         <link rel="icon" href="/favicon.ico" />
         <link
           href="https://fonts.googleapis.com/css?family=Poppins:400,600,700&display=swap"
